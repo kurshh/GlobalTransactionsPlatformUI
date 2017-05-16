e06e0a9155c6001dab46e0c6c4c6292c07bdd630
Petrauskas <S76076@corp1.ad1.seb.net>
2017-05-16 16:29:32 +0200

    Initial container springboot app for static content

---
+++ src/main/java/global/transactions/GlobalTransactionsPlatformUI.java
@@ -0,0 +1,12 @@
+package global.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalTransactionsPlatformUI {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalTransactionsPlatformUI.class, args);
+	}
+}
